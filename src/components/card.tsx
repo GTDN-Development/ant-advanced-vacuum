07ec4b19ec3034b3d1870dcd4bd226f29d15977f
Matěj Šebek <sebekmatej02@gmail.com>
2025-09-16 13:31:42 +0200

    upgraded cards

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,4 +1,3 @@
-import { Button } from "./button";
 import { ArrowRightIcon } from "./icons/arrow-right-icon";
 
 export function Card(
@@ -8,21 +7,24 @@ export function Card(
     <div
       {...props}
       className="[clip-path:polygon(0%_0%,_100%_0%,_100%_90%,_90%_100%,_0%_100%)]
-                 bg-[#00000012] w-full min-w-[316px] h-[464px] flex flex-col"
+                 bg-[#00000012] w-full flex flex-col p-6"
     >
       {props.src && (
-        <div className="p-6">
-          <img src={props.src} className="w-full h-[264px] object-cover" />
+        <div className="relative aspect-square overflow-clip">
+          <img
+            src={props.src}
+            className="absolute w-full object-cover top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+          />
         </div>
       )}
 
-      <h1 className="font-bold text-xl px-6 mb-4">{props.title}</h1>
+      <h1 className="font-bold text-xl">{props.title}</h1>
 
-      <div className="mt-auto px-6 pb-6 ">
-        <Button variant="noborder" icon="hasIcon">
+      <div className="mt-auto">
+        <button className="text-red-500 flex items-center gap-2">
+          More info
           <ArrowRightIcon aria-hidden="true" className="text-red-500 size-4" />
-          button
-        </Button>
+        </button>
       </div>
     </div>
   );
